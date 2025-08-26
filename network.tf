64dfcdfca050fd5ef9f26f605fd063ee411af77d
Daniel Niecke <daniel.niecke@worldline.com>
2025-08-26 17:45:45 +0200

    fix cidr

--- network.tf
+++ network.tf
@@ -1,13 +1,13 @@
 resource "google_compute_network" "doc_store_vpc" {
   project                 = var.project_id
-  name                    = "custom-vpc"
+  name                    = "doc-store-vpc"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "frankfurt_subnet" {
   project       = var.project_id
   name          = "frankfurt-subnet"
-  ip_cidr_range = "10.0.1.0/20"
+  ip_cidr_range = "10.0.0.0/20"
   region        = "europe-west3"            # Frankfurt
   network       = google_compute_network.doc_store_vpc.self_link
 }
@@ -15,7 +15,7 @@ resource "google_compute_subnetwork" "frankfurt_subnet" {
 resource "google_compute_subnetwork" "berlin_subnet" {
   project       = var.project_id
   name          = "berlin-subnet"
-  ip_cidr_range = "10.0.2.0/20"
+  ip_cidr_range = "10.0.16.0/20"
   region        = "europe-west10"           # Berlin
   network       = google_compute_network.doc_store_vpc.self_link
 }
